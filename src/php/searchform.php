56440671a268c225dcec7b87deec479ba52aa3ec
Mike Ball <mikedball@gmail.com>
2012-10-31 16:12:46 -0400

    Added custom searchform

---
+++ src/php/searchform.php
@@ -0,0 +1,4 @@
+<form role="search" method="get" class="searchform" action="<?php echo home_url( '/' ); ?>">
+  <input type="text" value="" name="s" />
+  <button type="submit" id="searchsubmit">Search</button>
+</form>
